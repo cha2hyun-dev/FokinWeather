f7b6d972a9ea28685828027cbc9b5a5d30ac2f40
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-02-17 18:27:14 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,30 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.yellowView}>
-                <Text>HELLO?</Text>
-            </View>
-            <View style={styles.blueView}>
-                <Text>HELLO!</Text>
-            </View>
-        </View>
-    );
+    return <Loading />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff"
-    },
-    yellowView: {
-        flex: 2,
-        backgroundColor: "yellow"
-    },
-    blueView: {
-        flex: 20,
-        backgroundColor: "blue"
-    }
-});
